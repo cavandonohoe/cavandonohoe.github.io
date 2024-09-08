744ae438eeb7bf67c0f5cf9820133cb82c004ac0
STEPHANIE HUSSEY <stephaniehussey@STEPHANIEs-MacBook-Air.local>
2024-09-08 11:22:39 +0100

    adding imdb rating plot to github

---
+++ web_scraping/imdb_season_episode_ratings_plot.R
@@ -0,0 +1,43 @@
+
+
+
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+library(RColorBrewer)
+library(rvest)
+
+# Function to scrape IMDb series episode data
+scrape_imdb_episode_data <- function(url) {
+  url <- url %>% stringr::str_remove_all("/$") %>% file.path("episodes")
+  webpage <- read_html(url)
+  
+  seasons <- webpage %>%
+    html_node("main > div > section > div > section > div > div.sc-4b498b6e-1.jDpRYv.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(2) > section.sc-6d19272a-0.bUgwdr > div.ipc-tabs.ipc-tabs--base.ipc-tabs--align-left.ipc-tabs--display-chip.ipc-tabs--inherit > ul") %>%
+    html_nodes("a") %>%
+    html_text()
+  
+  season_urls <- webpage %>%
+    html_node("#__next") %>%
+    html_node("main > div > section > div > section > div > div.sc-4b498b6e-1.jDpRYv.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(2) > section.sc-6d19272a-0.bUgwdr > div.ipc-tabs.ipc-tabs--base.ipc-tabs--align-left.ipc-tabs--display-chip.ipc-tabs--inherit > ul") %>%
+    html_nodes("a") %>%
+    html_attr("href") %>%
+    paste0("https://www.imdb.com", .)
+  
+  # Create the episode data frame
+  episode_data <- create_episode_data(season_urls)
+  
+  return(episode_data)
+}
+
+
+
+# game of thrones
+game_of_thrones_ratings = scrape_imdb_episode_data("https://www.imdb.com/title/tt0944947/")
+game_of_thrones_ratings %>% write_csv("data/game_of_thrones_ep_ratings.csv")
+# breaking bad
+breaking_bad_rating = scrape_imdb_episode_data("https://www.imdb.com/title/tt0903747/")
+breaking_bad_rating %>% write_csv("data/breaking_bad_ep_ratings.csv")
+
+
+
