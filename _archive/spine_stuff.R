9b884ee214fdf66339bf83da29ae60a9ddbd0766
Cavan Donohoe <cavandonohoe@gmail.com>
2025-12-28 23:09:19 -0800

    Apply repo-wide R style lint

--- _archive/spine_stuff.R
+++ _archive/spine_stuff.R
@@ -1,45 +1,60 @@
 `%>%` <- magrittr::`%>%`
-
-random_number = c(rep(.5, 30000),
+random_number <- c(rep(.5, 30000),
                   rnorm(n=30000, mean = .42, sd = .05),
                   rnorm(n=30000, mean = .18, sd = .05),
                   rnorm(n=30000, mean = -.22, sd = .05),
                   rnorm(n=30000, mean = -.78, sd = .05),
                   rnorm(n=30000, mean = -1.5, sd = .05))
 
-random_sample_tib %>% dplyr::filter(sample %in% 1:5) %>% 
+random_sample_tib %>%
+  dplyr::filter(sample %in% 1:5) %>%
+  
   ggplot2::ggplot(ggplot2::aes(x = position, y = random_number, color = sample)) +
   ggplot2::geom_point() +
   ggplot2::geom_hline(yintercept = 0) +
   ggplot2::geom_vline(xintercept = 0) +
   ggplot2::ggtitle("Sumedh's Million Dollar Graph")
 
-random_sample_tib = tibble::tibble(random_number) %>%
+random_sample_tib <- tibble::tibble(random_number) %>%
+  
   dplyr::mutate(sample = rep(seq(1, length(random_number) / 6), 6),
          # sample = (dplyr::row_number() - 1) %/% 6 + 1,
          position = (dplyr::row_number() - 1) %/% (length(random_number) / 6) + 1
          # position = rep(1:6, times = length(random_number) / 6)
          ) %>%
+           
   dplyr::arrange(sample, position) %>%
+    
   dplyr::group_by(sample) %>%
-  dplyr::mutate(sample_sum = sum(random_number)) %>% 
-  dplyr::ungroup() %>% 
+    
+  dplyr::mutate(sample_sum = sum(random_number)) %>%
+    
+  dplyr::ungroup() %>%
+    
   dplyr::group_by(sample) %>%
+    
   dplyr::mutate(theta = REdaS::rad2deg(tan((random_number - dplyr::lag(random_number)) /
-                                      (position - dplyr::lag(position))))) %>% 
+                                      (position - dplyr::lag(position))))) %>%
+                                        
   dplyr::ungroup()
 
 # average slope
-random_sample_tib %>% dplyr::group_by(position) %>% 
+random_sample_tib %>%
+  dplyr::group_by(position) %>%
+  
   dplyr::summarise(ave_theta = mean(theta, na.rm = TRUE))
 
-random_sample_tib %>% dplyr::pull(sample_sum) %>% unique
+random_sample_tib %>%
+  dplyr::pull(sample_sum) %>%
+  unique
 
 # sumedh wants there to be an integral in here
 # no need when each vertebrae is equal anyways right?
 # plus across different spines of different lengths, the integral is going to be different
 # for each spine
 
-random_sample_tib %>% dplyr::pull(sample_sum) %>% mean
+random_sample_tib %>%
+  dplyr::pull(sample_sum) %>%
+  mean
 
 
--- selling_vs_renting.R
+++ selling_vs_renting.R
@@ -1,36 +1,45 @@
 `%>%` <- magrittr::`%>%`
-
 ##### ASSUMPTIONS #####
-interest_rate = .1 # yearly interest rate
-n_periods = 100000 # load up the vector
-pmt = 5000
-value_of_house = 900000
+interest_rate <- .1 # yearly interest rate
+n_periods <- 100000 # load up the vector
+pmt <- 5000
+value_of_house <- 900000
 
-n_vector = seq(1, n_periods, by = 1)
+n_vector <- seq(1, n_periods, by = 1)
 
 ##### CALCULATION #####
 # annuity
-rented = FinCal::fv(r = interest_rate / 12, n = n_vector, pmt = -1*pmt, pv = 0, type = 0)
+rented <- FinCal::fv(r = interest_rate / 12, n = n_vector, pmt = -1*pmt, pv = 0, type = 0)
 
 
 # one time investment from getting house
-selling = value_of_house * (1 + interest_rate)^(n_vector/12)
+selling <- value_of_house * (1 + interest_rate)^(n_vector/12)
 
-check = tibble::tibble(n = n_vector, rented_value = rented, selling_value = selling)
+check <- tibble::tibble(n = n_vector, rented_value = rented, selling_value = selling)
 
 # let's do a fun thing where we check where the intersection is
-check2 = check %>% dplyr::mutate(rented_greater_than_selling = rented_value > selling_value)
+check2 <- check %>%
+  dplyr::mutate(rented_greater_than_selling = rented_value > selling_value)
 
-intersection = check2 %>% dplyr::filter(rented_greater_than_selling) %>% dplyr::pull(n) %>% min
+intersection <- check2 %>%
+  dplyr::filter(rented_greater_than_selling) %>%
+  dplyr::pull(n) %>%
+  min
 
-check3 = check2 %>% dplyr::filter(n < intersection * 2) %>% dplyr::select(-rented_greater_than_selling)
+check3 <- check2 %>%
+  dplyr::filter(n < intersection * 2) %>%
+  dplyr::select(-rented_greater_than_selling)
 
-check4 = check3 %>% tidyr::gather(key = rented_or_selling, value = amount, -n) %>%
+check4 <- check3 %>%
+  tidyr::gather(key = rented_or_selling, value = amount, -n) %>%
+  
   dplyr::arrange(n)
 
-check4 %>% ggplot2::ggplot(ggplot2::aes(x = n, y = amount, colour = rented_or_selling)) +
+check4 %>%
+  ggplot2::ggplot(ggplot2::aes(x = n, y = amount, colour = rented_or_selling)) +
   ggplot2::geom_line() +
-  ggplot2::scale_y_continuous(labels = scales::label_dollar(), breaks = scales::pretty_breaks(n = 12)) +
+  ggplot2::scale_y_continuous(labels = scales::label_dollar(),
+    breaks = scales::pretty_breaks(n = 12)) +
   ggplot2::xlab("n(months)") +
   ggplot2::theme(axis.title.y = ggplot2::element_blank())
 
