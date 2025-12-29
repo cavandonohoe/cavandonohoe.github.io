9051a43382ccf50e870d5fa432e4cd61dcd66ee6
cavandonohoe <cavandonohoe@users.noreply.github.com>
2025-12-29 07:19:15 +0000

    deploy: 92f67886af23fba279f24326b0a5a7f4f9714a9a

--- pr-24/web_scraping/top250_movies_with_rt.R
+++ pr-24/web_scraping/top250_movies_with_rt.R
@@ -1,23 +1,30 @@
 `%>%` <- magrittr::`%>%`
-
-
 # alright working on imdb top 250 movies now
 # maybe I can get the rotten tomatoes as well
 # I think check the director(s) and that might be your unique identifier
 
-url = "https://www.imdb.com/chart/top/"
+url <- "https://www.imdb.com/chart/top/"
 
-top_250_html = url %>% rvest::read_html()
+top_250_html <- url %>%
+  rvest::read_html()
 
-top_250_tib = top_250_html %>% 
-  rvest::html_node("#main > div > span > div > div > div.lister > table") %>% 
-  rvest::html_table() %>% tibble::as_tibble(.name_repair = "universal")
+top_250_tib <- top_250_html %>%
+  
+  rvest::html_node("#main > div > span > div > div > div.lister > table") %>%
+    
+  rvest::html_table() %>%
+    tibble::as_tibble(.name_repair = "universal")
 
-top_250_ids = top_250_html %>% 
-  rvest::html_nodes("#main > div > span > div > div > div.lister > table > tbody") %>% 
-  rvest::html_nodes("td.titleColumn > a") %>% 
+top_250_ids <- top_250_html %>%
+  
+  rvest::html_nodes("#main > div > span > div > div > div.lister > table > tbody") %>%
+    
+  rvest::html_nodes("td.titleColumn > a") %>%
+    
   rvest::html_attr("href") %>%
+    
   gsub(pattern = "/title/", replacement = "") %>%
+    
   gsub(pattern = "/.*", replacement = "")
 
 
@@ -25,24 +32,32 @@ top_250_ids = top_250_html %>%
 # so we can check if the rotten tomatoes directors match up with the imdb directors
 # our qc (quality check) will set our control group as the imdb group since I trust imdb more
 
-directors = top_250_html %>% 
-  rvest::html_nodes("#main > div > span > div > div > div.lister > table > tbody") %>% 
+directors <- top_250_html %>%
+  
+  rvest::html_nodes("#main > div > span > div > div > div.lister > table > tbody") %>%
+    
   rvest::html_nodes("td.titleColumn > a") %>%
+    
   rvest::html_attr("title") %>%
+    
   gsub(pattern = " \\(dir\\.\\).*", replacement = "")
 
 
-top_250_tib_clean = top_250_tib %>% 
+top_250_tib_clean <- top_250_tib %>%
+  
   dplyr::mutate(id = top_250_ids,
          rank = dplyr::row_number(),
          # extract text between last set of parentheses
          year = gsub(x = Rank...Title, pattern = ".*\\((.*)\\).*", replacement = "\\1") %>%
+           
            as.numeric(),
          # extract text from first and last space
          title = gsub(x = Rank...Title, pattern = "^\\S+\\s+|\\s+[^ ]+$", replacement = ""),
          site = paste0("https://www.imdb.com/title/", id),
          director = directors) %>%
+           
   dplyr::select(rank, title, director, year, imdb_rating = IMDb.Rating, id, site) %>%
+    
   dplyr::mutate(producer_site = file.path(site, "companycredits?ref_=ttfc_sa_3"))
 
 
@@ -56,13 +71,15 @@ top_250_tib_clean = top_250_tib %>%
 # also, I said they drop the 'the', but I mean... not always
 
 
-rotten_tomatoes_url = top_250_tib_clean %>% 
+rotten_tomatoes_url <- top_250_tib_clean %>%
+  
   dplyr::select(title, imdb_director = director) %>%
-  dplyr::mutate(rotten_tomatoes_title = 
+    
+  dplyr::mutate(rotten_tomatoes_title =
            dplyr::case_when((title == "The Dark Knight" & imdb_director == "Christopher Nolan") |
                        (title == "The Dark Knight Rises" & imdb_director == "Christopher Nolan") |
                        (title == "The Gold Rush" & imdb_director == "Charles Chaplin") |
-                       (title == "The Grand Budapest Hotel" & imdb_director == "Wes Anderson") ~ 
+                       (title == "The Grand Budapest Hotel" & imdb_director == "Wes Anderson") ~
                        snakecase::to_any_case(string = title, case = "snake"),
                      title == "12 Angry Men" & imdb_director == "Sidney Lumet" ~
                        "1000013_12_angry_men",
@@ -72,9 +89,11 @@ rotten_tomatoes_url = top_250_tib_clean %>%
                        "seven",
                      title == "Life Is Beautiful" & imdb_director == "Roberto Benigni" ~
                        "1084398-life_is_beautiful",
-                     title == "Star Wars: Episode IV - A New Hope" & imdb_director == "George Lucas" ~
+                     title == "Star Wars: Episode IV - A New Hope" &
+                       imdb_director == "George Lucas" ~
                        "star_wars",
-                     title == "Star Wars: Episode V - The Empire Strikes Back" & imdb_director == "Irvin Kershner" ~
+                     title == "Star Wars: Episode V - The Empire Strikes Back" &
+                       imdb_director == "Irvin Kershner" ~
                        "empire_strikes_back",
                      title == "Hara-Kiri" & imdb_director == "Masaki Kobayashi" ~
                        "harakiri",
@@ -82,11 +101,16 @@ rotten_tomatoes_url = top_250_tib_clean %>%
                        "whiplash_2014",
                      title == "Casablanca" & imdb_director == "Michael Curtiz" ~
                        "1003707-casablanca",
-                     title == "The Lives of Others" & imdb_director == "Florian Henckel von Donnersmarck" ~
+                     title == "The Lives of Others" &
+                       imdb_director == "Florian Henckel von Donnersmarck" ~
                        "the_lives_of_others",
                      title == "Sunset Blvd." & imdb_director == "Billy Wilder" ~
                        "sunset_boulevard",
-                     title == "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb" & imdb_director == "Stanley Kubrick" ~
+                     title == paste0(
+                       "Dr. Strangelove or: How I Learned to Stop Worrying and ",
+                       "Love the Bomb"
+                     ) &
+                       imdb_director == "Stanley Kubrick" ~
                        "dr_strangelove",
                      title == "Princess Mononoke" & imdb_director == "Hayao Miyazaki" ~
                        "princess_mononoke_1999",
@@ -206,11 +230,11 @@ rotten_tomatoes_url = top_250_tib_clean %>%
          rotten_tomatoes_url = paste0("https://www.rottentomatoes.com/m/", rotten_tomatoes_title))
 
 
-html_list_rot = list()
-index = 1
+html_list_rot <- list()
+index <- 1
 for (rot_url in rotten_tomatoes_url$rotten_tomatoes_url) {
   html_list_rot[[index]] = rvest::read_html(rot_url)
-  index = index + 1
+  index <- index + 1
 }
 # rotten_tomatoes_url %>% dplyr::filter(dplyr::row_number() == index)
 
@@ -218,11 +242,11 @@ for (rot_url in rotten_tomatoes_url$rotten_tomatoes_url) {
 # let's grab the imdb html data
 # unreliable producers are in rotten tomatoes
 # can definitely get more data from imdb afterwards too
-html_imdb_list = list()
-index = 1
+html_imdb_list <- list()
+index <- 1
 for (imdb_url in top_250_tib_clean$site) {
   html_imdb_list[[index]] = rvest::read_html(imdb_url)
-  index = index + 1
+  index <- index + 1
 }
 
 # I've found it might be easiest to do this in a for loop
@@ -234,32 +258,50 @@ for (imdb_url in top_250_tib_clean$site) {
 # fucking imdb...
 
 
-html_imdb_prod_list = list()
-index = 1
+html_imdb_prod_list <- list()
+index <- 1
 for (imdb_url in top_250_tib_clean$producer_site) {
   html_imdb_prod_list[[index]] = rvest::read_html(imdb_url)
-  index = index + 1
+  index <- index + 1
 }
 
 
 imdb_producers <- function(html_object) {
   html_object %>%
+    
     rvest::html_nodes("#company_credits_content > ul") %>%
+      
     rvest::html_text() %>%
+      
     .[1] %>%
+      
     stringr::str_split(pattern = "\n") %>%
-    unlist %>% stringr::str_trim() %>% stringr::str_squish() %>%
+      
+    unlist %>%
+      stringr::str_trim() %>%
+      stringr::str_squish() %>%
+      
     `[` (.!="") %>%
+      
     paste(collapse = "|")
 }
 
 # production_companies_imdb_fun = function(html_object) {
 #   html_object %>%
 #     rvest::html_nodes("#__next > main > div") %>% 
-#     rvest::html_nodes("section.ipc-page-background.ipc-page-background--base.TitlePage__StyledPageBackground-wzlr49-0.dDUGgO") %>% 
+#     rvest::html_nodes(paste0(
+#       "section.ipc-page-background.ipc-page-background--base.",
+#       "TitlePage__StyledPageBackground-wzlr49-0.dDUGgO"
+#     )) %>% 
 #     rvest::html_nodes("div > section > div") %>% 
-#     rvest::html_nodes("div.TitleMainBelowTheFoldGroup__TitleMainPrimaryGroup-sc-1vpywau-1.btXiqv.ipc-page-grid__item.ipc-page-grid__item--span-2") %>% 
-#     # rvest::html_nodes("section:nth-child(44) > div.styles__MetaDataContainer-sc-12uhu9s-0.cgqHBf > ul > li:nth-child(7)")
+#     rvest::html_nodes(paste0(
+#       "div.TitleMainBelowTheFoldGroup__TitleMainPrimaryGroup-sc-1vpywau-1.",
+#       "btXiqv.ipc-page-grid__item.ipc-page-grid__item--span-2"
+#     )) %>% 
+#     # rvest::html_nodes(paste0(
+#     #   "section:nth-child(44) > div.styles__MetaDataContainer-sc-12uhu9s-0.cgqHBf > ",
+#     #   "ul > li:nth-child(7)"
+#     # ))
 #     rvest::html_nodes("section") %>%
 #     # rvest::html_attr("data-testid")
 #     rvest::html_nodes(xpath = "//*[@data-testid='Details']") %>%
@@ -272,81 +314,119 @@ imdb_producers <- function(html_object) {
 
 
 
-audience_score_fun = function(html_object){
-  html_object %>% rvest::html_nodes("#topSection > div.thumbnail-scoreboard-wrap > score-board") %>%
+audience_score_fun <- function(html_object){
+  html_object %>%
+    rvest::html_nodes("#topSection > div.thumbnail-scoreboard-wrap > score-board") %>%
+    
     rvest::html_attr("audiencescore")
 }
 
-tomato_meter_score_fun = function(html_object){
-  html_object %>% rvest::html_nodes("#topSection > div.thumbnail-scoreboard-wrap > score-board") %>%
+tomato_meter_score_fun <- function(html_object){
+  html_object %>%
+    rvest::html_nodes("#topSection > div.thumbnail-scoreboard-wrap > score-board") %>%
+    
     rvest::html_attr("tomatometerscore")
 }
 
-directors_rot = function(html_object){
-  director = html_object %>%
-    rvest::html_nodes("section.panel.panel-rt.panel-box.movie_info.media > div > div > ul >li") %>% 
+directors_rot <- function(html_object){
+  director <- html_object %>%
+    
+    rvest::html_nodes("section.panel.panel-rt.panel-box.movie_info.media > div > div > ul >li") %>%
+      
     rvest::html_text() %>%
-    stringr::str_squish() %>% 
+      
+    stringr::str_squish() %>%
+      
     tibble::as_tibble() %>%
-    tidyr::separate(col = value, sep = ": ", into = c("title", "name")) %>% 
-    dplyr::filter(title == "Director") %>% dplyr::pull(name)
-  
+      
+    tidyr::separate(col = value, sep = ": ", into = c("title", "name")) %>%
+      
+    dplyr::filter(title == "Director") %>%
+      dplyr::pull(name)
+
   if (length(director) < 1) {
-    director = NA
+    director <- NA
   }
   return(director)
 }
 
-movie_info_rot = function(html_object){
-  movie_info = html_object %>%
-    rvest::html_nodes("section.panel.panel-rt.panel-box.movie_info.media > div > div > ul >li") %>% 
+movie_info_rot <- function(html_object){
+  movie_info <- html_object %>%
+    
+    rvest::html_nodes("section.panel.panel-rt.panel-box.movie_info.media > div > div > ul >li") %>%
+      
     rvest::html_text() %>%
-    stringr::str_squish() %>% 
+      
+    stringr::str_squish() %>%
+      
     tibble::as_tibble() %>%
+      
     tidyr::separate(col = value, sep = ": ", into = c("category", "name"))
-  
+
   return(movie_info)
-  
+
 }
 
 # run html node functions
 
-audience_scores = html_list_rot %>% 
-  lapply(audience_score_fun) %>% unlist()
+audience_scores <- html_list_rot %>%
+  
+  lapply(audience_score_fun) %>%
+    unlist()
 
 
-tomato_meter_scores = html_list_rot %>% 
-  lapply(tomato_meter_score_fun) %>% unlist()
+tomato_meter_scores <- html_list_rot %>%
+  
+  lapply(tomato_meter_score_fun) %>%
+    unlist()
 
-director_rot_tom = html_list_rot %>% 
-  lapply(directors_rot) %>% unlist()
+director_rot_tom <- html_list_rot %>%
+  
+  lapply(directors_rot) %>%
+    unlist()
 
-movie_info = html_list_rot %>% 
+movie_info <- html_list_rot %>%
+  
   lapply(movie_info_rot)
 names(movie_info) = top_250_tib_clean$title
 
-movie_info_tib = movie_info %>% dplyr::bind_rows(.id = "title")
+movie_info_tib <- movie_info %>%
+  dplyr::bind_rows(.id = "title")
 
-producers_imdb = html_imdb_prod_list %>% 
-  lapply(imdb_producers) %>% 
+producers_imdb <- html_imdb_prod_list %>%
+  
+  lapply(imdb_producers) %>%
+    
   unlist()
 
 producers_imdb <- producers_imdb %>%
+  
   # remove all parentheses and insides of parentheses
   stringr::str_remove_all("\\s*\\([^\\)]+\\)")
 
-producers_imdb = tibble::tibble(id = top_250_tib_clean$id, producers_imdb)
+producers_imdb <- tibble::tibble(id = top_250_tib_clean$id, producers_imdb)
 
-top_250_imdb_rot_tom = top_250_tib_clean %>% 
+top_250_imdb_rot_tom <- top_250_tib_clean %>%
+  
   dplyr::mutate(audience_scores = as.numeric(audience_scores),
          tomato_meter_scores = as.numeric(tomato_meter_scores),
-         director_rot_tom = director_rot_tom) %>% 
-  dplyr::left_join(movie_info_tib %>% dplyr::filter(category == "Director") %>% dplyr::select(title, director = name)) %>% 
-  dplyr::left_join(movie_info_tib %>% dplyr::filter(category == "Distributor") %>% dplyr::select(title, distributor = name)) %>%
-  dplyr::left_join(movie_info_tib %>% dplyr::filter(category == "Production Co") %>% dplyr::select(title, production_co = name)) %>% 
-  dplyr::mutate(produced_by = ifelse(is.na(distributor), production_co, distributor)) %>% 
+         director_rot_tom = director_rot_tom) %>%
+           
+  dplyr::left_join(movie_info_tib %>%
+    dplyr::filter(category == "Director") %>%
+    dplyr::select(title, director = name)) %>%
+    
+  dplyr::left_join(movie_info_tib %>%
+    dplyr::filter(category == "Distributor") %>%
+    dplyr::select(title, distributor = name)) %>%
+    
+  dplyr::left_join(movie_info_tib %>%
+    dplyr::filter(category == "Production Co") %>%
+    dplyr::select(title, production_co = name)) %>%
+    
+  dplyr::mutate(produced_by = ifelse(is.na(distributor), production_co, distributor)) %>%
+    
   dplyr::left_join(producers_imdb)
 
 
 # top_250_imdb_rot_tom %>% write.csv(here::here("data", "top250_movies_with_rt.csv"))
-
