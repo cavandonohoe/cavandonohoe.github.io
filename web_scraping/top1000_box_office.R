867a281ce7d8cf249345e91c9204fd2cc4d00f14
cavandonohoe <cavandonohoe@users.noreply.github.com>
2026-01-03 20:40:46 +0000

    deploy: c5f286f0c57495e8d3b67dcaf861f14138e5d721

--- web_scraping/top1000_box_office.R
+++ web_scraping/top1000_box_office.R
@@ -8,6 +8,35 @@
 # and continues till 800
 # let's get web scraping
 
+imdb_user_agent <- paste(
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+  "AppleWebKit/537.36 (KHTML, like Gecko)",
+  "Chrome/122.0.0.0 Safari/537.36"
+)
+
+read_imdb_html <- function(url) {
+  response <- httr::GET(
+    url,
+    httr::user_agent(imdb_user_agent),
+    httr::add_headers("Accept-Language" = "en-US,en;q=0.9")
+  )
+  httr::stop_for_status(response)
+  xml2::read_html(response)
+}
+
+imdb_next_data <- function(url) {
+  html <- read_imdb_html(url)
+  json_text <- html %>%
+    rvest::html_node("script#__NEXT_DATA__") %>%
+    rvest::html_text()
+
+  if (length(json_text) == 0) {
+    stop("IMDb page did not include __NEXT_DATA__.")
+  }
+
+  jsonlite::fromJSON(json_text, simplifyVector = TRUE, flatten = TRUE)
+}
+
 url <- "https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW"
 
 links <- rvest::read_html(url) %>%
@@ -25,7 +54,7 @@ for (url_index in all_urls) {
     rvest::html_node("table") %>%
     rvest::html_table(fill=TRUE) %>%
     tibble::as_tibble() %>%
-    dplyr::mutate(Rank = as.numeric(Rank))
+    dplyr::mutate(Rank = readr::parse_number(Rank))
 
   # funny enough, before doing this, I just read in a list of imdb movies titles 
   # and got the imdb id that way. but apparently multiple versions of beauty and the beast
@@ -57,30 +86,29 @@ rankings_tib_final <- rankings_tib %>%
 rankings_tib_final2 <- rankings_tib_final %>%
   dplyr::mutate(imdb_link = paste0("https://www.imdb.com/title/", id))
 
+if (!exists("refresh_ratings")) {
+  refresh_ratings <- FALSE
+}
 
-get_rating <- function(link){
-  link %>%
-    rvest::read_html() %>%
-    # old version
-    # rvest::html_nodes(
-    #   xpath = paste0(
-    #     "//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[1]/",
-    #     "div[2]/div/div[1]/a/div/div/div[2]/div[1]/span[1]"
-    #   )
-    # ) %>%
-    # new version
-    rvest::html_nodes(paste0(
-      "#__next > main > div > section.ipc-page-background.ipc-page-background--base.",
-      "sc-ca85a21c-0.efoFqn > section > div:nth-child(4) > section > section > ",
-      "div.sc-80d4314-0.fjPRnj > div.sc-db8c1937-0.eGmDjE.sc-80d4314-3.iBtAhY > ",
-      "div > div:nth-child(1) > a"
-    )) %>%
-    rvest::html_nodes("div > div") %>%
-    rvest::html_nodes(paste0(
-      "div.sc-7ab21ed2-0.fAePGh > div.sc-7ab21ed2-2.kYEdvH > ",
-      "span.sc-7ab21ed2-1.jGRxWM"
-    )) %>%
-    rvest::html_text()
+if (!exists("ratings_cache_path")) {
+  ratings_cache_path <- file.path("data", "top1000_imdb_ratings_cache.rds")
+}
+
+get_rating <- function(title_id) {
+  url <- paste0("https://www.imdb.com/title/", title_id, "/")
+  rating <- tryCatch({
+    page_data <- imdb_next_data(url)
+    rating_value <- page_data$props$pageProps$aboveTheFoldData$ratingsSummary$aggregateRating
+    if (is.null(rating_value) || length(rating_value) == 0) {
+      NA_real_
+    } else {
+      as.numeric(rating_value)
+    }
+  }, error = function(err) {
+    NA_real_
+  })
+
+  rating
 }
 
 
@@ -88,8 +116,47 @@ get_rating <- function(link){
 # look through 1000 websites and find each rating
 # luckily imdb has unique id's for each movie unlike a different movie rating website
 # *cough* rotten tomatoes *cough*
-all_user_ratings <- rankings_tib_final2$imdb_link %>%
-  parallel::mclapply(get_rating, mc.cores = parallel::detectCores() - 1)
+ratings_cache <- tibble::tibble(id = character(), imdb_rating = numeric())
+if (file.exists(ratings_cache_path) && !refresh_ratings) {
+  ratings_cache <- readRDS(ratings_cache_path)
+}
+
+ratings_lookup <- setNames(ratings_cache$imdb_rating, ratings_cache$id)
+title_lookup <- setNames(rankings_tib_final2$Title, rankings_tib_final2$id)
+missing_ids <- setdiff(rankings_tib_final2$id, names(ratings_lookup))
+
+if (length(missing_ids) > 0) {
+  recent_titles <- character()
+  for (i in seq_along(missing_ids)) {
+    title_id <- missing_ids[[i]]
+    title_name <- title_lookup[[title_id]]
+    ratings_lookup[[title_id]] <- get_rating(title_id)
+    recent_titles <- c(recent_titles, sprintf("%s (%s)", title_name, title_id))
+
+    if (i %% 10 == 0 || i == length(missing_ids)) {
+      message(sprintf("Fetched %d/%d:", i, length(missing_ids)))
+      message(paste(recent_titles, collapse = "\n"))
+      message("")
+      recent_titles <- character()
+    }
+
+    if (i %% 25 == 0) {
+      saveRDS(
+        tibble::tibble(id = names(ratings_lookup),
+                       imdb_rating = as.numeric(ratings_lookup)),
+        ratings_cache_path
+      )
+    }
+  }
+}
+
+saveRDS(
+  tibble::tibble(id = names(ratings_lookup),
+                 imdb_rating = as.numeric(ratings_lookup)),
+  ratings_cache_path
+)
+
+all_user_ratings <- as.numeric(ratings_lookup[rankings_tib_final2$id])
 
 
 
