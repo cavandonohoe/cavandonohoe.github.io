fb35e0628340a281f497f3f055ee6a87f8c17021
Cavan Donohoe <cavandonohoe@gmail.com>
2026-04-28 15:01:30 -0700

    Filter Chase Google Sheet to shared purchases

--- scripts/upload_chase_tx_to_gsheet.R
+++ scripts/upload_chase_tx_to_gsheet.R
@@ -13,6 +13,49 @@ read_workbook_sheet <- function(xlsx_path, sheet_name) {
   readxl::read_xlsx(xlsx_path, sheet = sheet_name)
 }
 
+sheet_id_by_name <- function(ss, sheet_name) {
+  sheets <- googlesheets4::gs4_get(ss)$sheets
+  match_idx <- match(sheet_name, sheets$name)
+  if (is.na(match_idx)) {
+    stop("Could not find sheet named ", sheet_name)
+  }
+  sheets$id[[match_idx]]
+}
+
+set_owner_b_filter <- function(ss, sheet_name, n_cols) {
+  owner_col_idx <- 5
+  criteria <- setNames(
+    list(
+      list(
+        hiddenValues = list("", "c")
+      )
+    ),
+    as.character(owner_col_idx)
+  )
+  request <- googlesheets4::request_generate(
+    "sheets.spreadsheets.batchUpdate",
+    params = list(
+      spreadsheetId = googlesheets4::as_sheets_id(ss),
+      requests = list(
+        list(
+          setBasicFilter = list(
+            filter = list(
+              range = list(
+                sheetId = sheet_id_by_name(ss, sheet_name),
+                startRowIndex = 0,
+                startColumnIndex = 0,
+                endColumnIndex = n_cols
+              ),
+              criteria = criteria
+            )
+          )
+        )
+      )
+    )
+  )
+  googlesheets4::request_make(request)
+}
+
 upload_sheet <- function(ss, sheet_name, data) {
   existing_sheets <- googlesheets4::sheet_names(ss)
   if (!(sheet_name %in% existing_sheets)) {
@@ -39,6 +82,7 @@ main <- function(args) {
   metadata <- read_workbook_sheet(xlsx_path, "Metadata")
 
   upload_sheet(ss, "Transactions", tx)
+  set_owner_b_filter(ss, "Transactions", ncol(tx))
   upload_sheet(ss, "Monthly B Summary", summary)
   upload_sheet(ss, "Metadata", metadata)
 
