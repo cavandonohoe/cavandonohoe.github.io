a147800bfb4646056c06f97ad51521a5a3ef6694
Cavan Donohoe <cavandonohoe@gmail.com>
2026-07-28 10:41:21 -0700

    Add Batman: Caped Crusader to IMDb rating plot

--- web_scraping/imdb_season_episode_ratings_plot.R
+++ web_scraping/imdb_season_episode_ratings_plot.R
@@ -339,7 +339,8 @@ shows <- tibble::tribble(
   "modern love",        "tt8543390",   "modern_love",
   "my hero academia",   "tt5626028",   "my_hero_academia",
   "the boys",           "tt1190634",   "the_boys",
-  "schitt's creek",     "tt3526078",   "schitts_creek"
+  "schitt's creek",     "tt3526078",   "schitts_creek",
+  "batman caped crusader", "tt14681596", "batman_caped_crusader"
 )
 
 # -----------------------------------------------------------------------------
