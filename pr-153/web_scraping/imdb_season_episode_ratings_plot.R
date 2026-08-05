0894a116a7c9401d5c2c88834ef618ddfbb2c7dd
cavandonohoe <cavandonohoe@users.noreply.github.com>
2026-08-05 16:26:11 +0000

    deploy: 1063689e18302d33298e6a6683eb6625fbefb0c6

--- pr-153/web_scraping/imdb_season_episode_ratings_plot.R
+++ pr-153/web_scraping/imdb_season_episode_ratings_plot.R
@@ -358,7 +358,12 @@ shows <- tibble::tribble(
   "daredevil",          "tt3322312",   "daredevil",
   "the mandalorian",    "tt8111088",   "the_mandalorian",
   "archer",             "tt1486217",   "archer",
-  "heated rivalry",     "tt35495073",  "heated_rivalry"
+  "heated rivalry",     "tt35495073",  "heated_rivalry",
+  "scrubs",             "tt0285403",   "scrubs",
+  "the last of us",     "tt3581920",   "the_last_of_us",
+  "barry",              "tt5348176",   "barry",
+  "community",          "tt1439629",   "community",
+  "futurama",           "tt0149460",   "futurama"
 )
 
 # -----------------------------------------------------------------------------
